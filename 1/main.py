c035ec8de97bc5a9c783512cb33ab22124f707b7
Tom Gregg <thomasgregg05@outlook.com>
2024-12-02 01:15:30 +0000

    first commit

---
+++ 1/main.py
@@ -0,0 +1,14 @@
+file = open("puzzle.txt", "r")
+data = [line.strip().split() for line in file]
+
+list1 = [int(row[0]) for row in data]
+list2 = [int(row[1]) for row in data]
+
+count = 0
+
+for num in list1:
+  for item in list2:
+    if(num == item):
+      count+= num
+
+print(count)
